a72ce7a222d799971fb5e9d294a5567029d9b666
Kritish <kritishdhungel@gmail.com>
2025-11-03 13:27:47 +0545

    Made Pre populated Slug

--- category/admin.py
+++ category/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Category
 
 # Register your models here.
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("categoryName",)}
+    list_display = ('categoryName', 'slug')
+
+admin.site.register(Category, CategoryAdmin)
--- pasal/settings.py
+++ pasal/settings.py
@@ -126,6 +126,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']   # points to your development static folder
 STATIC_ROOT = BASE_DIR / 'staticfiles'     # where collectstatic will dump files for production
 
+#Media Configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_DIR = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
